4ec4cb0910b93d36f578a217f69fa1e29c880083
m-malikov <malikov.ma@phystech.com>
2018-06-18 23:20:52 +0300

    icorating

--- icorating.py
+++ icorating.py
@@ -1,24 +1,37 @@
 from bs4 import BeautifulSoup
 import requests
 from pprint import pprint
+import re
 
 
 def get_ratings():
-    html_text = requests.get(
-        "https://icobench.com/icos?filterSort=rating-desc").text
-
-    soup = BeautifulSoup(html_text, 'html.parser')
-
     ratings = {}
-    for item in soup.select('div.ico_list tr'):
-        name_element = item.select('a.name')
-        rate_element = item.select('div.rate')
-        if name_element:
-            name = name_element[0].text
-            suffix = " (PreICO)"
-            if name.endswith(suffix):
-                name = name[:-len(suffix)]
-            rate = rate_element[0].text
-            rate = int(float(rate)*20)
-            ratings["name"] = rate
+    for address in ["/ico", "/ico/preico"]:
+        html_text = requests.get("https://icorating.com" + address).text
+
+        soup = BeautifulSoup(html_text, 'html.parser')
+
+        for item in soup.select('table')[1].select('tr'):
+            cols = item.select('td')
+            if len(cols) > 5:
+                name = cols[1].text.strip()
+                rate = cols[5].text.strip()
+                name = re.sub(r" \([\w]*\)", "", name)
+                grades = {
+                    "Positive+": 100,
+                    "Positive": 90,
+                    "Stable+": 80,
+                    "Stable": 70,
+                    "Risky+": 60,
+                    "Risky": 50,
+                    "Risky-": 40,
+                    "Negative": 30,
+                    "Negative-": 20,
+                    "Default": 10,
+                }
+                rate = grades[rate]
+                ratings[name] = rate
     return ratings
+
+
+print(get_ratings())
