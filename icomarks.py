21f5daa662d860fcc6f324bb4321427f2d402278
m-malikov <malikov.ma@phystech.com>
2018-06-19 00:53:37 +0300

    icomarks

---
+++ icomarks.py
@@ -0,0 +1,20 @@
+from bs4 import BeautifulSoup
+import requests
+from pprint import pprint
+
+
+def get_ratings():
+    ratings = {}
+    for status in ['presale_active', 'active']:
+        for offset in range(0, 200, 20):
+            html_text = requests.post(
+                "https://icomarks.com/icos/ajax_more", data={"offset": offset, "status": status}).json()["content"]
+            soup = BeautifulSoup(html_text, 'html.parser')
+            for item in soup.select('div.icoListItem'):
+                name = item.select('a.icoListItem__title')[0].text
+                rate = item.select('div.icoListItem__rate')[0].text.strip()
+                ratings[name] = int(float(rate) * 10)
+    return ratings
+
+
+print(len(get_ratings()))
